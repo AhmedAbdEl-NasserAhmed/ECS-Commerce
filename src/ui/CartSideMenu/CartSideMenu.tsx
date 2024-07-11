1a8514730c39108ac94432ba716120ed624c20cf
Ahmed Nasser <nasserahmed093@gmail.com>
2024-07-11 04:55:07 +0300

    finishing side menu

---
+++ src/ui/CartSideMenu/CartSideMenu.tsx
@@ -0,0 +1,172 @@
+import useClickOutside from "@/hooks/useClickOutside";
+import {
+  decrementProductItem,
+  incrementProductItem,
+  removeItem,
+} from "@/lib/features/cartSlice/cartSlice";
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+import { CartItem } from "@/types/types";
+import Image from "next/image";
+import toast from "react-hot-toast";
+
+function CartSideMenu({ setOpenSideMenu, openSideMenu }) {
+  const cart = useAppSelector((state) => state.cartSlice.cartItems);
+
+  const ref = useClickOutside({ close: setOpenSideMenu, value: false });
+
+  const dispatch = useAppDispatch();
+
+  function handleDeleteProduct(color) {
+    dispatch(removeItem(color));
+  }
+
+  function handleIncrementProductQuantity(product) {
+    if (product.quantity !== product.maxQuantity) {
+      dispatch(
+        incrementProductItem({
+          id: product.id,
+          maxQuantity: product.maxQuantity,
+        })
+      );
+    } else {
+      toast.error(" This is maximum Quantity for this product Color");
+    }
+  }
+
+  function handleDecrementProductQuantity(product) {
+    dispatch(decrementProductItem(product.id));
+  }
+
+  const totalCartItems = cart.reduce((acc, cur) => {
+    return acc + cur.quantity * cur.price;
+  }, 0);
+
+  return (
+    <div
+      className={`fixed top-0 ${
+        openSideMenu ? "left-0" : "-left-[550px]"
+      } transition-all duration-300  h-screen w-full backdrop-filter backdrop-blur-sm z-50  `}
+    >
+      <div ref={ref} className="bg-white w-[70vw] h-full p-6 overflow-y-scroll">
+        {cart.length === 0 ? (
+          <div className="text-black">Cart is Empty</div>
+        ) : (
+          <>
+            <h2 className="text-2xl font-semibold text-black capitalize mb-10">
+              Shopping Cart
+            </h2>
+            {/* LIST */}
+            <div className="flex flex-col gap-14">
+              {cart.map((product: CartItem) => {
+                return (
+                  <div key={product.id} className="flex gap-4">
+                    {true && (
+                      <Image
+                        src={product.image}
+                        alt=""
+                        width={72}
+                        height={96}
+                        className="object-cover rounded-xl"
+                      />
+                    )}
+                    <div className="flex flex-col justify-between w-full">
+                      {/* TOP */}
+                      <div className="text-xl">
+                        {/* TITLE */}
+                        <div className="flex items-center justify-between gap-8 text-black">
+                          <h3 className="font-semibold capitalize">
+                            {product.name}
+                          </h3>
+                          <div className="p-1 bg-gray-50 rounded-xl flex items-center gap-2">
+                            {true && (
+                              <div className="text-sm font-semibold text-green-500">
+                                {product.quantity} x{" "}
+                              </div>
+                            )}
+                            {product.price}
+                          </div>
+                        </div>
+                        {/* DESC */}
+                        <div className="flex justify-between  gap-4">
+                          <div className="text-xl text-gray-500">
+                            Size: {product.size}
+                          </div>
+                          <div className=" flex items-center gap-4 text-xl  text-gray-500">
+                            <span>Color: </span>
+                            <span
+                              className=" w-5 h-5 rounded-full"
+                              style={{ backgroundColor: product.color }}
+                            >
+                              &nbsp;
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                      {/* BOTTOM */}
+                      <div className="flex justify-between items-center text-xl">
+                        <span className="text-gray-500">
+                          Qty. {product.quantity}
+                        </span>
+
+                        <div className="flex items-center gap-5 flex-col min-[370px]:flex-row ">
+                          <button
+                            className={` ${
+                              product.quantity === product.maxQuantity
+                                ? "bg-red-500"
+                                : ""
+                            } w-7 h-7 bg-black rounded-full text-white flex items-center justify-center`}
+                            onClick={() =>
+                              handleIncrementProductQuantity(product)
+                            }
+                          >
+                            +
+                          </button>
+                          <button
+                            className="w-7 h-7 bg-black rounded-full text-white flex items-center justify-center"
+                            onClick={() =>
+                              handleDecrementProductQuantity(product)
+                            }
+                          >
+                            -
+                          </button>
+                        </div>
+
+                        <span
+                          className="text-blue-500 cursor-pointer"
+                          onClick={() => handleDeleteProduct(product.id)}
+                        >
+                          Remove
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+
+            {/* BOTTOM */}
+            <div className="mt-10">
+              <div className="flex items-center justify-between font-bold text-xl text-black">
+                <span className="">Total</span>
+                <span className="">{totalCartItems} EGP</span>
+              </div>
+              <p className="text-gray-500 text-xl mt-2 mb-4">
+                Shipping and taxes calculated at checkout.
+              </p>
+              <div className="flex justify-between text-xl">
+                <button className="rounded-xl py-3 px-4 ring-1 ring-gray-300 text-black">
+                  View Cart
+                </button>
+                <button className="rounded-xl py-3 px-4 bg-black text-white disabled:cursor-not-allowed disabled:opacity-75">
+                  Checkout
+                </button>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default CartSideMenu;

b8da48803e09edbde7bef331a03dd17ab965fcad
Khaled Yousef <K.Yousef-c@tamkeentech.sa>
2024-07-31 22:09:02 +0300

    add social media links

--- src/app/[locale]/user/product/[slug]/page.tsx
+++ src/app/[locale]/user/product/[slug]/page.tsx
@@ -352,7 +352,7 @@ function ProductDetails() {
       <Head>
         <meta
           property="og:image"
-          // content="https://res.cloudinary.com/practicaldev/image/fetch/s--0qQ47wTC--/c_imagga_scale,f_auto,fl_progressive,h_500,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/wcg9dbu0z2ca1e9dxy1g.png"
+          content="https://res.cloudinary.com/practicaldev/image/fetch/s--0qQ47wTC--/c_imagga_scale,f_auto,fl_progressive,h_500,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/wcg9dbu0z2ca1e9dxy1g.png"
         />
 
         <meta property="og:type" content="article" />

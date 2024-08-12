73a4b9e529050f8c1f1edbfd9820982188e58a3e
Khaled Yousef <K.Yousef-c@tamkeentech.sa>
2024-08-12 20:48:22 +0300

    add medium to sizes

--- src/lib/StaticLookups.ts
+++ src/lib/StaticLookups.ts
@@ -1,6 +1,7 @@
 export const sizes = [
   { id: "XS", value: "XS", label: "XS", color: "#666666" },
   { id: "SM", value: "SM", label: "SM", color: "#666666" },
+  { id: "M", value: "M", label: "M", color: "#666666" },
   { id: "L", value: "L", label: "L", color: "#666666" },
   { id: "XL", value: "XL", label: "XL", color: "#666666" },
   { id: "XXL", value: "XXL", label: "XXL", color: "#666666" },
